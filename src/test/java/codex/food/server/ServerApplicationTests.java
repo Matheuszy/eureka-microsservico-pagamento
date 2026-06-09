e268d71bbb070234e2d5b399d32b7e37dfc16897
Matheus Carlos <169002682+Matheuszy@users.noreply.github.com>
2026-06-08 21:36:23 -0300

    Add files via upload

---
+++ src/test/java/codex/food/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package codex.food.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
